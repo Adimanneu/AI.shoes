7605e5cc9e02d9c0b7ce6f23f3ac48256867b5e2
isramash <esraamashaal99@gmail.com>
2023-11-27 17:48:22 +0100

    Created java-script file + css-file

---
+++ components/structure_isra/structure_isra.js
@@ -0,0 +1,29 @@
+
+function render_structure ()
+{
+  const container = document.querySelector("#wrapper");
+  container.innerHTML = `
+    <header>
+        <nav id= "nav_bar">
+            <a>ALL</a>
+            <a>BOOTS</a>
+            <a>SNEAKERS</a>
+            <a>TOFFLOR</a>
+            <a>ABOUT US</a>
+        </nav>
+    </header>
+    
+    <footer></footer>
+  `;
+
+  return {
+    header: container.querySelector("header"),
+    main: container.querySelector("main"),
+    nav: container.querySelector("#nav_bar"),
+    top: container.querySelector("#top"),
+    bottom: container.querySelector("#bottom"),
+  };
+}
+const body = document.body;
+render_structure (body);
+
