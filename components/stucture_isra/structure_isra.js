6b056ff79376018c2c426d51d37d722693eda415
isramash <esraamashaal99@gmail.com>
2023-11-27 18:09:32 +0100

    create new elements

--- components/stucture_isra/structure_isra.js
+++ components/stucture_isra/structure_isra.js
@@ -1,7 +1,28 @@
 function render_structure ()
 {
   const container = document.querySelector("#wrapper");
-  
+  container.innerHTML = `
+    <header>
+        <nav id= "nav_bar">
+            <a>ALL</a>
+            <a>BOOTS</a>
+            <a>SNEAKERS</a>
+            <a>TOFFLOR</a>
+            <a>ABOUT US</a>
+        </nav>
+    </header>
+    <main>
+        <div id="top">
+            <h1><h1>
+            <p></p>
+        </div>
+        <div id="bottom">
+        </div>
+      
+    </main>
+    
+    <footer></footer>
+  `;
 
   return {
     header: container.querySelector("header"),
