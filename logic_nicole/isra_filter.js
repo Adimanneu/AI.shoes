4061ec27622022c60609c988c08a8070c36f540b
Nicole Jonsson <143434132+ninijonsson@users.noreply.github.com>
2023-12-06 14:47:30 +0100

    Delete logic_nicole directory

--- logic_nicole/isra_filter.js
+++
@@ -1,9 +0,0 @@
-function isra_filter(arrayOfshoes, x , y){
-    let filter = [];
-    for (let shoe of arrayOfshoes){
-        if (shoe.price >= x && shoe.price <= y){
-            filter.push(shoe);
-        }
-    }
-    return filter;
-}
