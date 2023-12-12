2f67bfc9ffbe8b32b66a2f3d0f731721a489ac24
hildahammer <hilda_hammer@yahoo.se>
2023-12-12 22:35:26 +0100

    fixed underline for header

--- components/header/header.js
+++ components/header/header.js
@@ -33,6 +33,15 @@ function f1(event) {
     Explore our impressive collection of boots - the ultimate fusion of style and functionality. Our boots are designed to take you through every season and occasion, from rugged and durable work boots to trendy and elegant city styles. Crafted with premium materials and meticulous craftsmanship, our boots offer you both comfort and lasting style. Whether you prefer classic leather, modern textures, or sporty details, we have a wide selection to suit your personal taste. From adventures in nature to urban escapades, our boots will take you there in style.
     </p>
     `;
+
+    let underline_selected = document.querySelector(".selected");
+    if (underline_selected !== null) {
+        underline_selected.classList.toggle("selected");
+    }
+
+    let underline_boots = document.querySelector(".boots");
+    underline_boots.classList.toggle("selected");
+
 }
 
 function f2(event) {
@@ -41,6 +50,15 @@ function f2(event) {
     <h1 class="shoe_rubrik">SNEAKERS</h1>
     <p class="info_shoe">Discover our fantastic range of sneakers - a perfect blend of comfort and trend. Our sneakers are crafted to fit every style and occasion, from casual everyday adventures to sporty performances. With high-quality materials and modern design, our sneakers offer both style and support for your feet. Whether you prefer classic colors or dare to go bold with patterns and details, we have a broad range to match your personal taste. Step forward in style and comfort with our sneakers, whether you're taking a stroll around the block or embracing new challenges on the go.</p>
     `;
+
+
+    let underline_selected = document.querySelector(".selected");
+    if (underline_selected !== null) {
+        underline_selected.classList.toggle("selected");
+    }
+
+    let underline_sneakers = document.querySelector(".sneakers");
+    underline_sneakers.classList.toggle("selected");
 }
 
 function f3(event) {
@@ -49,6 +67,14 @@ function f3(event) {
     <h1 class="shoe_rubrik">SLIPPERS</h1>
     <p class="info_shoe">Welcome to our collection of slippers - your perfect companions for relaxation and comfort. Our slippers are created to give your feet a haven of comfort after a long day. Made with soft materials and ergonomic design, our slippers provide a sense of luxury and respite for your tired feet. Whether you prefer warm and fluffy options for winter or cool and airy for summer, we offer a wide selection that caters to your preferences and needs. Relax at home in style and enjoy every step you take in our comfortable and stylish slippers.</p>
     `;
+
+    let underline_selected = document.querySelector(".selected");
+    if (underline_selected !== null) {
+        underline_selected.classList.toggle("selected");
+    }
+
+    let underline_slippers = document.querySelector(".slippers");
+    underline_slippers.classList.toggle("selected");
 }
 
 function f4(event) {
@@ -56,6 +82,14 @@ function f4(event) {
     n.innerHTML = `
     <h1 class="shoe_rubrik">ALL</h1>
     `;
+
+    let underline_selected = document.querySelector(".selected");
+    if (underline_selected !== null) {
+        underline_selected.classList.toggle("selected");
+    }
+
+    let underline_all = document.querySelector(".all");
+    underline_all.classList.toggle("selected");
 }
 
 function f5(event) {
@@ -78,6 +112,11 @@ function f5(event) {
 
     let closeButton = n.querySelector(".about_popup_closebutton");
     closeButton.addEventListener("click", f6);
+
+    let underline_selected = document.querySelector(".selected");
+    if (underline_selected !== null) {
+        underline_selected.classList.toggle("selected");
+    }
 }
 
 function f6(event) {
