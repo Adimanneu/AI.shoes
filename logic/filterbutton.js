ade01e2067bba61b79d8424b238388e15f6671b6
Gitgubben1 <david.hedin1999@gmail.com>
2023-12-22 09:47:58 +0100

    Update filterbutton.js

--- logic/filterbutton.js
+++ logic/filterbutton.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -19,31 +19,30 @@ document.addEventListener('DOMContentLoaded', function () {
   filterPopup.classList.add('filter-popup');
 
   filterPopup.innerHTML = `
-    <div class="filter-content">
-      <span class="close-button">x</span>
-      <h1>FILTER</h1>
-      <div class="filter-content-types">
-        <div>
-          <h3> Types of Shoes</h3>
-          <label for="Boots"><input id="Boots" class="input-box type-checkbox" type="checkbox">Boots </label>
-          <label for="Sneakers"><input id="Sneakers" class="input-box type-checkbox" type="checkbox">Sneakers </label><br>
-          <label for="Tofflor"><input id="Tofflor" class="input-box type-checkbox" type="checkbox">Tofflor </label>
-        </div>
-        <div>
-          <h3> Max price </h3>
-          <p> Up to: <input type="text" class="input-price"> KR </p>
-        </div>
-        <div>
-          <h3> Made In</h3>
-          <label for="Spain"><input id="Spain" class="input-box country-checkbox" type="checkbox">Spain </label>
-          <label for="Portugal"><input id="Portugal" class="input-box country-checkbox" type="checkbox"> Portugal </label><br>
-          <label for="Kingdom"><input id="Kingdom" class="input-box country-checkbox" type="checkbox">United Kingdom </label><br>
-          <label for="Sweden"><input id="Sweden" class="input-box country-checkbox" type="checkbox">Sweden </label>
-        </div>
-      </div>
-      <button class="apply-filter-button">Apply Filter</button>
-    </div>
-  `;
+      <div class="filter-content">
+        <span class="close-button">x</span>
+        <h1>FILTER</h1>
+        <div class="filter-content-types">
+          <div>
+            <h3> Types of Shoes</h3>
+            <label for="Boots"><input id="Boots" class="input-box" type="checkbox">Boots </label>
+            <label for="Sneakers"><input id="Sneakers" class="input-box" type="checkbox">Sneakers </label><br>
+            <label for="Tofflor"><input id="Tofflor" class="input-box" type="checkbox">Tofflor </label>
+          </div>
+          <div>
+            <h3> Max price </h3>
+            <p> Up to: <input type="text" class="input-price"> KR </p>
+          </div>
+          <div>
+            <h3> Made In</h3>
+            <label for="Spain"><input id="Spain" class="input-box" type="checkbox">Spain </label>
+            <label for="Portugal"><input id="Portugal" class="input-box" type="checkbox"> Portugal </label><br>
+            <label for="Kingdom"><input id="Kingdom" class="input-box" type="checkbox">United Kingdom </label><br>
+            <label for="Sweden"><input id="Sweden" class="input-box" type="checkbox">Sweden </label>
+          </div>
+          <button class="apply-filter-button">Apply Filter</button>
+          </div>
+        `;
 
   document.body.appendChild(filterPopup);
 
