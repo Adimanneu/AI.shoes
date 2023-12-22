85e1fce1b28d5bf5edaba8d3eec940b628f401da
Gitgubben1 <david.hedin1999@gmail.com>
2023-12-22 09:32:22 +0100

    Max pris
    Går nu på första sidan ska bara fixa så det fungerar på de individuella genom naven. Även se till så att filterbutton inte försvinner när man trycker in max priset.

--- logic/filterbutton.js
+++ logic/filterbutton.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -19,29 +19,31 @@ document.addEventListener('DOMContentLoaded', function () {
   filterPopup.classList.add('filter-popup');
 
   filterPopup.innerHTML = `
-      <div class="filter-content">
-        <span class="close-button">x</span>
-        <h1>FILTER</h1>
-        <div class="filter-content-types">
-          <div>
-            <h3> Types of Shoes</h3>
-            <label for="Boots"><input id="Boots" class="input-box" type="checkbox">Boots </label>
-            <label for="Sneakers"><input id="Sneakers" class="input-box" type="checkbox">Sneakers </label><br>
-            <label for="Tofflor"><input id="Tofflor" class="input-box" type="checkbox">Tofflor </label>
-          </div>
-          <div>
-            <h3> Max price </h3>
-            <p> Up to: <input type="text" class="input-price"> KR </p>
-          </div>
-          <div>
-            <h3> Made In</h3>
-            <label for="Spain"><input id="Spain" class="input-box" type="checkbox">Spain </label>
-            <label for="Portugal"><input id="Portugal" class="input-box" type="checkbox"> Portugal </label><br>
-            <label for="Kingdom"><input id="Kingdom" class="input-box" type="checkbox">United Kingdom </label><br>
-            <label for="Sweden"><input id="Sweden" class="input-box" type="checkbox">Sweden </label>
-          </div>
+    <div class="filter-content">
+      <span class="close-button">x</span>
+      <h1>FILTER</h1>
+      <div class="filter-content-types">
+        <div>
+          <h3> Types of Shoes</h3>
+          <label for="Boots"><input id="Boots" class="input-box type-checkbox" type="checkbox">Boots </label>
+          <label for="Sneakers"><input id="Sneakers" class="input-box type-checkbox" type="checkbox">Sneakers </label><br>
+          <label for="Tofflor"><input id="Tofflor" class="input-box type-checkbox" type="checkbox">Tofflor </label>
         </div>
-    `;
+        <div>
+          <h3> Max price </h3>
+          <p> Up to: <input type="text" class="input-price"> KR </p>
+        </div>
+        <div>
+          <h3> Made In</h3>
+          <label for="Spain"><input id="Spain" class="input-box country-checkbox" type="checkbox">Spain </label>
+          <label for="Portugal"><input id="Portugal" class="input-box country-checkbox" type="checkbox"> Portugal </label><br>
+          <label for="Kingdom"><input id="Kingdom" class="input-box country-checkbox" type="checkbox">United Kingdom </label><br>
+          <label for="Sweden"><input id="Sweden" class="input-box country-checkbox" type="checkbox">Sweden </label>
+        </div>
+      </div>
+      <button class="apply-filter-button">Apply Filter</button>
+    </div>
+  `;
 
   document.body.appendChild(filterPopup);
 
@@ -53,6 +55,46 @@ document.addEventListener('DOMContentLoaded', function () {
   closeButton.addEventListener('click', function () {
     filterPopup.style.display = 'none';
   });
+
+  const applyFilterButton = filterPopup.querySelector('.apply-filter-button');
+  applyFilterButton.addEventListener('click', function () {
+    const maxPriceInput = document.querySelector('.input-price');
+    const maxPrice = parseFloat(maxPriceInput.value);
+
+    if (!isNaN(maxPrice)) {
+      const selectedTypes = getSelectedCheckboxes('.type-checkbox');
+      const selectedCountries = getSelectedCheckboxes('.country-checkbox');
+
+      const filteredShoes = filterShoes(SHOES, selectedTypes, 0, maxPrice, selectedCountries);
+      displayShoes(filteredShoes);
+    }
+
+    filterPopup.style.display = 'none';
+  });
+
+  function getSelectedCheckboxes(selector) {
+    const checkboxes = document.querySelectorAll(selector);
+    const selectedValues = Array.from(checkboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.id);
+    return selectedValues;
+  }
+
+  function filterShoes(arrayOfShoes, selectedTypes, minPrice, maxPrice, selectedCountries) {
+    return arrayOfShoes.filter(shoe => {
+      const meetsTypeCriteria = selectedTypes.length === 0 || selectedTypes.includes(shoe.kind_id.toString());
+      const meetsPriceCriteria = shoe.price >= minPrice && shoe.price <= maxPrice;
+      const meetsCountryCriteria = selectedCountries.length === 0 || selectedCountries.includes(shoe.country_id.toString());
+
+      return meetsTypeCriteria && meetsPriceCriteria && meetsCountryCriteria;
+    });
+  }
+
+  function displayShoes(shoes) {
+    const structureContainers = renderStructure();
+    const shoeListContainer = structureContainers.bottom;
+    render_header(structureContainers.header);
+    renderShoeList(shoeListContainer, shoes);
+  }
+
 });
-
-
